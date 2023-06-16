86d93ec5bd99a4d68da4a9eac6518e81f497a3dc
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-06-16 14:31:57 -0700

    editing

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 0 to 9 and letters between a to f.
+ * main - Prints numbers between 0 to 9.
  *
  * Return: Always 0 (Success)
  */
@@ -10,10 +10,6 @@ int main(void)
 	int i;
 
 	for (i = 48; i < 58; i++)
-	{
-		putchar(i);
-	}
-	for (i = 97; i < 103; i++)
 	{
 		putchar(i);
 	}
